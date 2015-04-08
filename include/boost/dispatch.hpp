83030897ef98b73c1894064f4615687628b01fd0
Joel Falcou <joel.falcou@lri.fr>
2015-04-08 20:22:32 +0200

    Main dispacth.hpp headers

---
+++ include/boost/dispatch.hpp
@@ -0,0 +1,46 @@
+//==================================================================================================
+/*!
+  @file
+
+  Provides a kitchen sink include files for the Boost.Dispatch library
+
+  @copyright 2009 - 2012 LASMEA UMR 6602 CNRS/Univ. Clermont II
+  @copyright 2009 - 2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
+  @copyright 2012 - 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+**/
+//==================================================================================================
+#ifndef BOOST_DISPATCH_HPP_INCLUDED
+#define BOOST_DISPATCH_HPP_INCLUDED
+
+namespace boost { namespace dispatch
+{
+  namespace concept
+  {
+    /*!
+      @defgroup group-concept Concepts
+      Concepts defined by the library
+    **/
+  }
+
+  namespace meta
+  {
+    /*!
+      @defgroup group-meta Meta-programming Utilities
+      Meta-programming related components
+    **/
+  }
+
+  namespace detail
+  {
+    /*!
+      @defgroup group-details Implementation details
+      Implementation details related components
+    **/
+  }
+
+} }
+#endif
