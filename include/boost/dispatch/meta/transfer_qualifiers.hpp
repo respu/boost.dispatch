c950c2938f0b6c17edbe6124b05cfc407a2fbefc
Joel Falcou <joel.falcou@lri.fr>
2015-04-08 20:23:29 +0200

    Make use of newly set type_traits placeholder file

--- include/boost/dispatch/meta/transfer_qualifiers.hpp
+++ include/boost/dispatch/meta/transfer_qualifiers.hpp
@@ -16,29 +16,58 @@
 #ifndef BOOST_DISPATCH_META_TRANSFER_QUALIFIERS_HPP_INCLUDED
 #define BOOST_DISPATCH_META_TRANSFER_QUALIFIERS_HPP_INCLUDED
 
-#include <type_traits>
+#include <boost/dispatch/details/type_traits.hpp>
 
-namespace boost { namespace dispatch { namespace meta
+namespace boost { namespace dispatch
 {
-  template<class T, class U>
-  struct transfer_qualifiers
+  namespace meta
   {
-    typedef T type;
-  };
+    /*!
+      @ingroup group-meta
+      @brief Transfer qualifiers between types
 
-  template<class T, class U>
-  struct transfer_qualifiers<T, U&&> : std::add_rvalue_reference<T> {};
+      Modify a type so it bears the same cref-qualifier than another type while following the
+      reference collapsing rules (<a href="http://isocpp.org/blog/2012/11/universal-references-in-c11-scott-meyers">
+      as described in this article</a>) which state that:
 
-  template<class T, class U>
-  struct transfer_qualifiers<T, U&> : std::add_lvalue_reference<T> {};
+      - An rvalue reference to an rvalue reference becomes (“collapses into”) an rvalue reference.
+      - All other references to references (i.e., all combinations involving an lvalue reference)
+        collapse into an lvalue reference.
 
-  template<class T, class U>
-  struct transfer_qualifiers<T, U const> : std::add_const<T> {};
+      @par Model:
 
-  template<class T, class U>
-  struct  transfer_qualifiers<T, U const&>
-        : std::add_lvalue_reference<typename std::add_const<T>::type>
-  {};
-} } }
+      @metafunction
+
+      @tparam T Type to modify
+      @tparam U Type which modifier needs to be transfered
+    **/
+    template<typename T, typename U>
+    struct transfer_qualifiers
+    {
+      typedef T type;
+    };
+
+    template<typename T, typename U>
+    struct transfer_qualifiers<T, U&&> : std::add_rvalue_reference<T> {};
+
+    template<typename T, typename U>
+    struct transfer_qualifiers<T, U&> : std::add_lvalue_reference<T> {};
+
+    template<typename T, typename U>
+    struct transfer_qualifiers<T, U const> : std::add_const<T> {};
+
+    template<typename T, typename U>
+    struct  transfer_qualifiers<T, U const&>
+          : std::add_lvalue_reference<typename std::add_const<T>::type>
+    {};
+  }
+
+  /*!
+    @ingroup group-meta
+    @brief C++14 style short-cut for meta::transfer_qualifiers
+  **/
+  template<typename T, typename U>
+  using transfer_qualifiers_t = typename meta::transfer_qualifiers<T,U>::type;
+} }
 
 #endif
