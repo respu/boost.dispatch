5acc0d146fc2b8d938fc7b7cab2587e7534f38ec
Joel Falcou <joel.falcou@lri.fr>
2015-06-12 20:06:11 +0200

    Add factory_of metafunction

---
+++ include/boost/dispatch/meta/detail/factory_of.hpp
@@ -0,0 +1,42 @@
+//==================================================================================================
+/*!
+  @file
+
+  Defines the meta::factory_of meta-function
+
+  @copyright 2009 - 2012 LASMEA UMR 6602 CNRS/Univ. Clermont II
+  @copyright 2009 - 2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
+  @copyright 2012 - 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+**/
+//==================================================================================================
+#ifndef BOOST_DISPATCH_META_DETAIL_FACTORY_OF_HPP_INCLUDED
+#define BOOST_DISPATCH_META_DETAIL_FACTORY_OF_HPP_INCLUDED
+
+#include <boost/dispatch/meta/model_of.hpp>
+#include <boost/dispatch/meta/value_of.hpp>
+#include <boost/dispatch/meta/primitive_of.hpp>
+
+namespace boost { namespace dispatch { namespace detail
+{
+  template<typename T, typename U> struct factory_of
+  {
+    using v_t = boost::dispatch::value_of_t<T>;
+
+    template<typename X>
+    using f_t = typename detail::factory_of<v_t,U>::template type<X>;
+
+    template<typename X>
+    using type = typename dispatch::meta::model_of<T>::template type<f_t<X>>;
+  };
+
+  template<typename T> struct factory_of<T, T>
+  {
+    template<typename X> using type = X;
+  };
+} } }
+
+#endif
--- include/boost/dispatch/meta/factory_of.hpp
+++ include/boost/dispatch/meta/factory_of.hpp
@@ -16,30 +16,9 @@
 #ifndef BOOST_DISPATCH_META_FACTORY_OF_HPP_INCLUDED
 #define BOOST_DISPATCH_META_FACTORY_OF_HPP_INCLUDED
 
-#include <boost/dispatch/meta/model_of.hpp>
-#include <boost/dispatch/meta/value_of.hpp>
-#include <boost/dispatch/meta/primitive_of.hpp>
+#include <boost/dispatch/meta/detail/factory_of.hpp>
 
-namespace boost { namespace dispatch { namespace detail
-{
-  template<typename T, typename U> struct factory_of
-  {
-    using v_t = boost::dispatch::value_of_t<T>;
-
-    template<typename X>
-    using f_t = typename detail::factory_of<v_t,U>::template type<X>;
-
-    template<typename X>
-    using type = typename dispatch::meta::model_of<T>::template type<f_t<X>>;
-  };
-
-  template<typename T> struct factory_of<T, T>
-  {
-    template<typename X> using type = X;
-  };
-}
-
-namespace meta
+namespace boost { namespace dispatch { namespace meta
 {
   /*!
     @ingroup group-meta
