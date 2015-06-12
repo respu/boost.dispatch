b77d3175b019bcdd51a580ce9579a83c6d1fadce
Joel Falcou <joel.falcou@lri.fr>
2015-06-12 15:21:44 +0200

    Use brigand library as internal detail

---
+++ include/boost/dispatch/detail/brigand/arithmetic.hpp
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include <brigand/plus.hpp>
+#include <brigand/minus.hpp>
+#include <brigand/times.hpp>
+#include <brigand/divides.hpp>
+#include <brigand/max.hpp>
+#include <brigand/min.hpp>
+#include <brigand/modulo.hpp>
+#include <brigand/next.hpp>
+#include <brigand/prev.hpp>
+#include <brigand/negate.hpp>
+#include <brigand/complement.hpp>
---
+++ include/boost/dispatch/detail/brigand/list.hpp
@@ -0,0 +1,9 @@
+
+#pragma once
+
+namespace brigand
+{
+  template <class... T> struct list {};
+
+  using empty_list = brigand::list<>;
+}
