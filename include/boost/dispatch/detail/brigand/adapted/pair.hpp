46a82c7f054db62c52eb2f861481efd28361e8e9
Joel Falcou <joel.falcou@lri.fr>
2015-06-14 18:54:08 +0200

    Updated brigand

--- include/boost/dispatch/detail/brigand/adapted/pair.hpp
+++ include/boost/dispatch/detail/brigand/adapted/pair.hpp
@@ -1,12 +1,30 @@
+/*==================================================================================================
+  Copyright (c) 2015 QuasarDB
+  Copyright (c) 2015 NumScale
 
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+=================================================================================================**/
 #pragma once
 
 #include <brigand/apply.hpp>
+#include <brigand/no_such_type.hpp>
 
 namespace brigand
 {
-  template <typename T, typename U>
+  template <typename... T>
   struct pair_wrapper
+  {
+    static_assert (sizeof...(T) == 2
+                  , "as_pair requires a type list of exactly two types"
+                  );
+
+    // type need to be defined or the error becomes a hard, non-static assert error
+    using type = no_such_type_;
+  };
+
+  template <typename T, typename U>
+  struct pair_wrapper<T,U>
   {
       using type = typename std::pair<T,U>;
   };
