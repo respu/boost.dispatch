30995cdd68cadf482fa2001551559de201d18dbd
Joel Falcou <joel.falcou@lri.fr>
2015-06-28 20:33:59 +0200

    Implement make_integer

---
+++ include/boost/dispatch/meta/make_integer.hpp
@@ -0,0 +1,105 @@
+//==================================================================================================
+/*!
+  @file
+
+  Defines the make_integer meta-function
+
+  @copyright 2009 - 2012 LASMEA UMR 6602 CNRS/Univ. Clermont II
+  @copyright 2009 - 2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
+  @copyright 2012 - 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+**/
+//==================================================================================================
+#ifndef BOOST_DISPATCH_META_MAKE_INTEGER_HPP_INCLUDED
+#define BOOST_DISPATCH_META_MAKE_INTEGER_HPP_INCLUDED
+
+#include <boost/dispatch/detail/brigand/functions/identity.hpp>
+#include <boost/dispatch/detail/brigand/types/no_such_type.hpp>
+#include <cstdint>
+#include <cstddef>
+
+namespace boost { namespace dispatch
+{
+  namespace detail
+  {
+    template< std::size_t Size, typename Sign, template<class> class Transform>
+    struct  make_integer
+    {
+      static_assert ( Size >= sizeof(std::int8_t) && Size <= sizeof(std::int64_t)
+                    , "boost::dispatch::make_integer: can't generate type of given Size"
+                    );
+
+      using type = brigand::no_such_type_;
+    };
+
+    template<template<class> class Transform>
+    struct make_integer<1u, unsigned, Transform>
+    {
+      using type = Transform<std::uint8_t>;
+    };
+
+    template<template<class> class Transform>
+    struct make_integer<2u, unsigned, Transform>
+    {
+      using type = Transform<std::uint16_t>;
+    };
+
+    template<template<class> class Transform>
+    struct make_integer<4u, unsigned, Transform>
+    {
+      using type = Transform<std::uint32_t>;
+    };
+
+    template<template<class> class Transform>
+    struct make_integer<8u, unsigned, Transform>
+    {
+      using type = Transform<std::uint64_t>;
+    };
+
+    template<template<class> class Transform>
+    struct make_integer<1u, signed, Transform>
+    {
+      using type = Transform<std::int8_t>;
+    };
+
+    template<template<class> class Transform>
+    struct make_integer<2u, signed, Transform>
+    {
+      using type = Transform<std::int16_t>;
+    };
+
+    template<template<class> class Transform>
+    struct make_integer<4u, signed, Transform>
+    {
+      using type = Transform<std::int32_t>;
+    };
+
+    template<template<class> class Transform>
+    struct make_integer<8u, signed, Transform>
+    {
+      using type = Transform<std::int64_t>;
+    };
+  }
+
+  /*!
+    @ingroup group-meta
+    @brief Generate an integer type of a given size and sign
+
+    For any given Size corresponding to a valid integer type and an optional Sign,
+    return said type optionally transformed by a user-defined unary meta-function.
+
+    @tparam Size      Size in bytes of the requested type
+    @tparam Sign      Optional sign (unsigned by default) of the requested type.
+    @tparam Transform Optional unary meta-function to apply to the generated type
+  **/
+  template< std::size_t Size
+          , class Sign                      = unsigned
+          , template<class> class Transform = brigand::identity
+          >
+  using make_integer = typename detail::make_integer<Size,Sign,Transform>::type;
+} }
+
+#endif
---
+++ test/meta/make_integer.cpp
@@ -0,0 +1,44 @@
+//==================================================================================================
+/*
+  Copyright 2009 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+  Copyright 2009 - 2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
+  Copyright 2012 - 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/dispatch/meta/make_integer.hpp>
+
+#include <nstest/unit.hpp>
+#include <nstest/unit/tests/types.hpp>
+
+NSTEST_CASE( "Generate raw integer types")
+{
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<1>)          , std::uint8_t    );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<2>)          , std::uint16_t   );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<4>)          , std::uint32_t   );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<8>)          , std::uint64_t   );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<1,unsigned>) , std::uint8_t    );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<2,unsigned>) , std::uint16_t   );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<4,unsigned>) , std::uint32_t   );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<8,unsigned>) , std::uint64_t   );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<1,signed>)   , std::int8_t     );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<2,signed>)   , std::int16_t    );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<4,signed>)   , std::int32_t    );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<8,signed>)   , std::int64_t    );
+}
+
+template<typename T> using ptr_t  =T*;
+
+NSTEST_CASE( "Generate transformed integer types")
+{
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<1,unsigned ,ptr_t>), std::uint8_t*   );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<2,unsigned ,ptr_t>), std::uint16_t*  );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<4,unsigned ,ptr_t>), std::uint32_t*  );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<8,unsigned ,ptr_t>), std::uint64_t*  );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<1,signed   ,ptr_t>), std::int8_t*    );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<2,signed   ,ptr_t>), std::int16_t*   );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<4,signed   ,ptr_t>), std::int32_t*   );
+  NSTEST_TYPE_IS( (boost::dispatch::make_integer<8,signed   ,ptr_t>), std::int64_t*   );
+}
