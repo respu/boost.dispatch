61a7314688f9f8b5d49b7516ef01b2d5f20504b0
Joel Falcou <joel.falcou@lri.fr>
2015-07-07 22:12:49 +0200

    aply_sign metafunction

---
+++ test/meta/apply_sign.cpp
@@ -0,0 +1,35 @@
+//==================================================================================================
+/*
+  Copyright 2009 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+  Copyright 2009 - 2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
+  Copyright 2012 - 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/dispatch/meta/generation/apply_sign.hpp>
+#include <boost/dispatch/meta/generation/as_unsigned.hpp>
+#include <boost/dispatch/meta/generation/as_signed.hpp>
+#include <cstdint>
+
+#include <nstest/unit.hpp>
+#include <nstest/unit/tests/types.hpp>
+
+NSTEST_CASE_TPL ( "Applying signed gives signed types"
+                , (std::uint8_t)(std::uint16_t)(std::uint32_t)(std::uint64_t)
+                  (std::int8_t)(std::int16_t)(std::int32_t)(std::int64_t)
+                  (float)(double)
+                )
+{
+  NSTEST_TYPE_IS( (boost::dispatch::apply_sign<T,signed>), boost::dispatch::as_signed<T> );
+}
+
+NSTEST_CASE_TPL ( "Applying unsigned gives signed types"
+                , (std::uint8_t)(std::uint16_t)(std::uint32_t)(std::uint64_t)
+                  (std::int8_t)(std::int16_t)(std::int32_t)(std::int64_t)
+                  (float)(double)
+                )
+{
+  NSTEST_TYPE_IS( (boost::dispatch::apply_sign<T,unsigned>), boost::dispatch::as_unsigned<T> );
+}
