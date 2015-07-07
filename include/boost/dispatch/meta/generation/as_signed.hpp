26e792265f73e56139a627224a91f9186562f83f
Joel Falcou <joel.falcou@lri.fr>
2015-07-07 20:16:34 +0200

    as_signed metafunction

---
+++ include/boost/dispatch/meta/generation/as_signed.hpp
@@ -0,0 +1,57 @@
+//==================================================================================================
+/*!
+  @file
+
+  Defines the as_signed meta-function
+
+  @copyright 2009 - 2012 LASMEA UMR 6602 CNRS/Univ. Clermont II
+  @copyright 2009 - 2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
+  @copyright 2012 - 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+**/
+//==================================================================================================
+#ifndef BOOST_DISPATCH_META_GENERATION_AS_SIGNED_HPP_INCLUDED
+#define BOOST_DISPATCH_META_GENERATION_AS_SIGNED_HPP_INCLUDED
+
+#include <boost/dispatch/meta/introspection/factory_of.hpp>
+#include <boost/dispatch/meta/introspection/primitive_of.hpp>
+#include <boost/dispatch/meta/introspection/is_natural.hpp>
+#include <type_traits>
+
+namespace boost { namespace dispatch
+{
+  namespace detail
+  {
+    template<typename T, typename Enable = void>
+    struct as_signed
+    {
+      using f_t  = boost::dispatch::factory_of<T>;
+      using p_t  = boost::dispatch::primitive_of<T>;
+      using type = typename f_t::template apply<typename detail::as_signed<p_t>::type>;
+    };
+
+    template<typename T>
+    struct as_signed<T, typename std::enable_if<std::is_fundamental<T>::value>::type>
+    {
+      template<typename U> struct id { using type = T;};
+      using result  = std::conditional<is_natural<T>::value,std::make_signed<T>,id<T>>;
+      using type    = typename result::type::type;
+    };
+  }
+
+  /*!
+    @ingroup group-meta
+    @brief Turn a type into an equivalent signed type
+
+    Converts any type which Primitive is Fundamental into a compatible signed type.
+
+    @tparam T     Type to convert.
+  **/
+  template<typename T>
+  using as_signed = typename detail::as_signed<typename std::decay<T>::type>::type;
+} }
+
+#endif
---
+++ test/meta/as_signed.cpp
@@ -0,0 +1,54 @@
+//==================================================================================================
+/*
+  Copyright 2009 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+  Copyright 2009 - 2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
+  Copyright 2012 - 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/dispatch/meta/generation/as_signed.hpp>
+#include <cstdint>
+
+#include <nstest/unit.hpp>
+#include <nstest/unit/tests/types.hpp>
+
+NSTEST_CASE_TPL ( "Generate signed from signed types"
+                , (float)(double)(bool)(std::int8_t)(std::int16_t)(std::int32_t)(std::int64_t)
+                )
+{
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<T>)       , T );
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<T&>)      , T );
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<T&&>)     , T );
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<T const>) , T );
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<T const&>), T );
+}
+
+template<typename T> using signed_t = typename std::make_signed<T>::type;
+
+NSTEST_CASE_TPL ( "Generate signed from unsigned types"
+                , (std::uint8_t)(std::uint16_t)(std::uint32_t)(std::uint64_t)
+                )
+{
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<T>)        ,(signed_t<T>) );
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<T&>)       ,(signed_t<T>) );
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<T&&>)      ,(signed_t<T>) );
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<T const>)  ,(signed_t<T>) );
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<T const&&>),(signed_t<T>) );
+}
+
+template<typename T> struct foo {};
+
+NSTEST_CASE_TPL ( "Generate signed from custom template types"
+                , (float)(double)(bool)
+                  (std::uint8_t)(std::uint16_t)(std::uint32_t)(std::uint64_t)
+                  (std::int8_t)(std::int16_t)(std::int32_t)(std::int64_t)
+                )
+{
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<foo<foo<T>>>)        , foo<foo<boost::dispatch::as_signed<T>>>);
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<foo<foo<T>>&>)       , foo<foo<boost::dispatch::as_signed<T>>>);
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<foo<foo<T>>&&>)      , foo<foo<boost::dispatch::as_signed<T>>>);
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<foo<foo<T>> const>)  , foo<foo<boost::dispatch::as_signed<T>>>);
+  NSTEST_TYPE_IS( (boost::dispatch::as_signed<foo<foo<T>> const&&>), foo<foo<boost::dispatch::as_signed<T>>>);
+}
