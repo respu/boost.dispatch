31ce160a7a7c126943f2195ac45f31107021f0dd
Joel Falcou <joel.falcou@lri.fr>
2015-09-19 19:13:48 +0200

    Macros for overload dispatch generation

---
+++ include/boost/dispatch/function/overload.hpp
@@ -0,0 +1,30 @@
+//==================================================================================================
+/*!
+  @file
+
+  Provides macros for function overload definition and declaration
+
+  @copyright 2009 - 2012 LASMEA UMR 6602 CNRS/Univ. Clermont II
+  @copyright 2009 - 2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
+  @copyright 2012 - 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+**/
+//==================================================================================================
+#ifndef BOOST_DISPATCH_FUNCTION_OVERLOAD_HPP_INCLUDED
+#define BOOST_DISPATCH_FUNCTION_OVERLOAD_HPP_INCLUDED
+
+#include <boost/preprocessor/cat.hpp>
+
+/*!
+  @ingroup group-api
+
+  Provides the name of the dispatching function for a given @c TAG
+
+  @param TAG Tag of the function to dispatch
+**/
+#define BOOST_DISPATCH_DISPATCHING_FUNCTION(TAG) BOOST_PP_CAT(dispatching_, TAG)
+
+#endif
