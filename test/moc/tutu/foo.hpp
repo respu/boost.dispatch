61f3397bea5bb7c1417a2e67648dac9988d30c5d
Joel Falcou <joel.falcou@lri.fr>
2015-10-08 18:24:23 +0200

    Fixed dispatch test

--- test/moc/tutu/foo.hpp
+++ test/moc/tutu/foo.hpp
@@ -19,7 +19,7 @@ namespace tutu { namespace titi
   namespace tag { BOOST_DISPATCH_MAKE_TAG(ext,foo_,boost::dispatch::elementwise_<foo_>); }
   namespace ext { BOOST_DISPATCH_FUNCTION_DECLARATION(tag,foo_); }
 
-  BOOST_DISPATCH_FUNCTION_DEFINITION(tag::foo_,foo);
+  BOOST_DISPATCH_CALLABLE_DEFINITION(tag::foo_,foo);
 } }
 
 #include "moc/arch/default/foo.hpp"
